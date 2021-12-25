85e4ba570b014eef26db0c48091d1ba029399278
Daisuke Sato <dsato2@nait.ca>
2021-12-24 18:18:37 -0700

    xmassssss

---
+++ 04_JavaScriptの基礎を固める/090_DOMにイベントを登録してみよう/start/main.js
@@ -0,0 +1,11 @@
+document.querySelector('#btn');
+
+
+const hello = function() {
+    alert ('hello');
+}
+
+btn.addEventListener('click',hello);
+// btn.addEventListener('click', function(){
+//     alert('hello');
+// });
